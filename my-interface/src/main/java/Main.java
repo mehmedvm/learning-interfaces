ae5c6f0d97d6f37c32d7bfeb83b4499fe359f350
MehmedVM <mehmedvm@gmail.com>
2022-04-01 12:33:54 -0400

    updated interface classes

--- my-interface/src/main/java/Main.java
+++ my-interface/src/main/java/Main.java
@@ -1,5 +1,25 @@
+import interfaces.Shape;
+import shapes.Circle;
+import shapes.Rectangle;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+
+        //Coding to an interface and not the implementation
+        Shape shape = new Circle(10);
+
+        //Drawing the shape
+        shape.draw();
+        System.out.println("Area of circle = " + shape.getArea());
+
+        //Switching from one implementation to another easily
+        shape = new Rectangle(10,10);
+        shape.draw();
+        System.out.println("Areaof a Rectangle = " + shape.getArea());
+
+        //Default in the interface
+        shape.defaultTest();
+
     }
 }
